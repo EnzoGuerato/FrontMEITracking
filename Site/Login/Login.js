e4317e62bf6f192afc48cc384fb488930a61b93a
--username-- <--email-->
2026-05-03 15:43:26 -0300

    Acessibilidade feita

--- Site/Login/Login.js
+++ Site/Login/Login.js
@@ -1,23 +1,50 @@
-const trilho = document.getElementById('trilho');
-const body = document.querySelector('body');
-const imgLua = document.getElementById('img-lua'); 
+const trilho = document.getElementById("trilho");
+const body = document.querySelector("body");
+const imgLua = document.getElementById("img-lua");
 
-const sol = "/Site/imgs/trilho/iconsol(preto).png";
-const lua = "/Site/imgs/trilho/iconlua(branca).png";
+const sol = "/Site/Login/logo meizinho/trilho/iconsol(preto).png";
+const lua = "/Site/Login/logo meizinho/trilho/iconlua(branca).png";
 
-trilho.addEventListener('click', () => {
-    trilho.classList.toggle('dark');
-    body.classList.toggle('dark');
+trilho.addEventListener("click", () => {
+  trilho.classList.toggle("dark");
+  body.classList.toggle("dark");
 
-    if (body.classList.contains('dark')) {
-        imgLua.src = sol;
-    } else {
-        imgLua.src = lua;
-    }
+  if (body.classList.contains("dark")) {
+    imgLua.src = sol;
+  } else {
+    imgLua.src = lua;
+  }
 });
 
-const links = document.getElementById('link')
+function validar() {
+  const valor = document.getElementById("email").value;
+  const aviso = document.getElementById("aviso");
 
-if(links != ""){
+  if (!valor.includes("@")) {
+    aviso.textContent = "e-mail não valido";
+  } else {
+    aviso.textContent = "";
+    console.log("Valor válido:", valor);
+  }
+}
 
+function validarsenha() {
+  const valorsenha = document.getElementById("senha").value;
+  const avisosenha = document.getElementById("avisosenha");
+
+  const testeMaius = /[A-Z]/.test(valorsenha);
+  const testEspecial = /[!@#$%^&*(),.?":{}|<>]/.test(valorsenha);
+
+  if (!testeMaius) {
+    avisosenha.textContent = "A senha deve conter uma letra maiúscula.";
+    return false;
+  }
+
+  if (!testEspecial) {
+    avisosenha.textContent = "A senha deve ter caracteres especiais.";
+    return false;
+  }
+
+  avisosenha.textContent = "";
+  return true;
 }
