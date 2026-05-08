60915d645695401fcb30bf9c02fc445f090951c7
felipesobrinhovergiliosv-pixel <felipesobrinhovergiliosv@gmail.com>
2026-05-08 11:44:52 -0300

    home

--- Site/home/Home.js
+++ Site/home/Home.js
@@ -2,8 +2,8 @@ const trilho = document.getElementById("trilho");
 const body = document.querySelector("body");
 const imgLua = document.getElementById("img-lua");
 
-const sol = "/Site/Login/logo meizinho/trilho/iconsol(preto).png";
-const lua = "/Site/Login/logo meizinho/trilho/iconlua(branca).png";
+const sol = "/Site/img/logo meizinho/trilho/iconsol(preto).png";
+const lua = "/Site/img/logo meizinho/trilho/iconlua(branca).png";
 
 trilho.addEventListener("click", () => {
   trilho.classList.toggle("dark");
@@ -15,36 +15,3 @@ trilho.addEventListener("click", () => {
     imgLua.src = lua;
   }
 });
-
-function validar() {
-  const valor = document.getElementById("email").value;
-  const aviso = document.getElementById("aviso");
-
-  if (!valor.includes("@")) {
-    aviso.textContent = "e-mail não valido";
-  } else {
-    aviso.textContent = "";
-    console.log("Valor válido:", valor);
-  }
-}
-
-function validarsenha() {
-  const valorsenha = document.getElementById("senha").value;
-  const avisosenha = document.getElementById("avisosenha");
-
-  const testeMaius = /[A-Z]/.test(valorsenha);
-  const testEspecial = /[!@#$%^&*(),.?":{}|<>]/.test(valorsenha);
-
-  if (!testeMaius) {
-    avisosenha.textContent = "A senha deve conter uma letra maiúscula.";
-    return false;
-  }
-
-  if (!testEspecial) {
-    avisosenha.textContent = "A senha deve ter caracteres especiais.";
-    return false;
-  }
-
-  avisosenha.textContent = "";
-  return true;
-}
