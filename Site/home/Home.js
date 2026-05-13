3dd96647f1af140521c5cf8fced158d3eb36f211
felipesobrinhovergiliosv-pixel <felipesobrinhovergiliosv@gmail.com>
2026-05-13 08:54:34 -0300

    home

--- Site/home/Home.js
+++ Site/home/Home.js
@@ -15,3 +15,28 @@ trilho.addEventListener("click", () => {
     imgLua.src = lua;
   }
 });
+
+//banner mobile//
+
+function trocarBanners() {
+
+  const banners = document.querySelectorAll(".banner-img");
+
+  banners.forEach((banner) => {
+
+    const isMobile = window.innerWidth <= 999;
+
+    const mobile = banner.getAttribute("data-mobile");
+    const desktop = banner.getAttribute("data-desktop");
+
+    banner.src = isMobile ? mobile : desktop;
+
+  });
+
+}
+
+/* roda só quando tudo estiver carregado */
+window.addEventListener("DOMContentLoaded", trocarBanners);
+
+/* troca ao redimensionar */
+window.addEventListener("resize", trocarBanners);
