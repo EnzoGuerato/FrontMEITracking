9e87b5af4b5f279ad6e6bd21055aaafbdf7945a4
Miguel Pelegrin <miguelpelegrin9@gmail.com>
2026-05-22 11:00:42 -0300

    Criação pastinhas do recsenha

---
+++ banco.sql
@@ -0,0 +1,10 @@
+CREATE TABLE senha;
+
+use senha;
+
+create table usuario(
+    id INT AUTO_INCREMENT PRIMARY KEY.
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(20)
+)
